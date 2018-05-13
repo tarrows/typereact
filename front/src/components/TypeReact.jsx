000aaa3197e7dc86cd00824e1575c53bd01b314e
c10t <c10t@fa.ke>
2018-05-13 15:27:43 +0900

    add & delete feature

--- front/src/components/TypeReact.jsx
+++ front/src/components/TypeReact.jsx
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import {notes} from "../actions";
+
 export class TypeReact extends Component {
+  state = {
+    text: ""
+  }
+
+  submitNote = (e) => {
+    e.preventDefault();
+    this.props.addNote(this.state.text);
+    this.setState({text: ""});
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to TypeReact!</h2>
         <hr />
 
+        <h3>Add new note</h3>
+        <form onSubmit={this.submitNote}>
+          <input
+            value={this.state.text}
+            placeholder="Enter note here..."
+            onChange={(e) => this.setState({text: e.target.value})}
+            required
+          />
+          <input type="submit" value="Save Note" />
+        </form>
+
         <h3>Notes</h3>
         <table>
-          <body>
-            {this.props.notes.map(note => (
-              <tr>
+          <tbody>
+            {this.props.notes.map((note, id) => (
+              <tr key={`note_${id}`}>
                 <td>{note.text}</td>
                 <td><button>edit</button></td>
-                <td><button>delete</button></td>
+                <td><button onClick={() => this.props.deleteNote(id)}>delete</button></td>
               </tr>
             ))}
-          </body>
+          </tbody>
         </table>
       </div>
     )
@@ -34,6 +57,15 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
+    addNote: (text) => {
+      dispatch(notes.addNote(text));
+    },
+    updateNote: (id, text) => {
+      dispatch(notes.updateNote(id, text));
+    },
+    deleteNote: (id) => {
+      dispatch(notes.deleteNote(id));
+    },
   }
 }
 
--- front/src/reducers/notes.js
+++ front/src/reducers/notes.js
@@ -4,7 +4,22 @@ const initialState = [
 
 
 export default function notes(state=initialState, action) {
+  let noteList = state.slice();
+
   switch (action.type) {
+    case 'ADD_NOTE':
+      return [...state, {text: action.text}];
+
+    case 'UPDATE_NOTE':
+      let noteToUpdate = noteList[action.id]
+      noteToUpdate.text = action.text;
+      noteList.splice(action.id, 1, noteToUpdate);
+      return noteList;
+
+    case 'DELETE_NOTE':
+      noteList.splice(action.id, 1);
+      return noteList;
+
     default:
       return state;
   }
