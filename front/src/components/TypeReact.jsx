e065c75d1772454f91afa1adb0bb49bd30ab7209
c10t <c10t@fa.ke>
2018-05-13 17:01:37 +0900

    edit feature

--- front/src/components/TypeReact.jsx
+++ front/src/components/TypeReact.jsx
@@ -5,13 +5,27 @@ import {notes} from "../actions";
 
 export class TypeReact extends Component {
   state = {
-    text: ""
+    text: "",
+    updateNoteId: null,
+  }
+
+  resetForm = () => {
+    this.setState({text: "", updateNoteId: null});
+  }
+
+  selectForEdit = (id) => {
+    let note = this.props.notes[id];
+    this.setState({text: note.text, updateNoteId: id});
   }
 
   submitNote = (e) => {
     e.preventDefault();
-    this.props.addNote(this.state.text);
-    this.setState({text: ""});
+    if (this.state.updateNoteId === null) {
+      this.props.addNote(this.state.text);
+    } else {
+      this.props.updateNote(this.state.updateNoteId, this.state.text);
+    }
+    this.resetForm();
   }
 
   render() {
@@ -28,6 +42,7 @@ export class TypeReact extends Component {
             onChange={(e) => this.setState({text: e.target.value})}
             required
           />
+          <button onClick={this.resetForm}>Reset</button>
           <input type="submit" value="Save Note" />
         </form>
 
@@ -37,7 +52,7 @@ export class TypeReact extends Component {
             {this.props.notes.map((note, id) => (
               <tr key={`note_${id}`}>
                 <td>{note.text}</td>
-                <td><button>edit</button></td>
+                <td><button onClick={() => this.selectForEdit(id)}>edit</button></td>
                 <td><button onClick={() => this.props.deleteNote(id)}>delete</button></td>
               </tr>
             ))}
