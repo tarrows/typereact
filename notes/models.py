8ae157ec4beac388abeae9ba52ffb47e726d33e9
c10t <c10t@fa.ke>
2018-05-13 20:59:46 +0900

    build api with django-rest-framework

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    text = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
--- typereact/urls.py
+++ typereact/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
 from django.views.generic import TemplateView
 
+from notes import endpoints
+
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
+    url(r'^api/', include(endpoints)),
     url(r'^', TemplateView.as_view(template_name="index.html")),
 ]
