5b9013a2161ae87810e8cafadb51b18430dafae5
c10t <c10t@fa.ke>
2018-05-24 07:45:53 +0900

    create login API

--- notes/api.py
+++ notes/api.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from knox.models import AuthToken
 
 from .models import Note
-from .serializers import NoteSerializer, CreateUserSerializer, UserSerializer
+from .serializers import NoteSerializer, CreateUserSerializer, UserSerializer, LoginUserSerializer
 
 
 class NoteViewSet(viewsets.ModelViewSet):
@@ -20,6 +20,19 @@ class RegistrationAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)
+        })
+
+
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
         return Response({
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)
--- notes/endpoints.py
+++ notes/endpoints.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from .api import NoteViewSet, RegistrationAPI
+from .api import NoteViewSet, RegistrationAPI, LoginAPI
 
 router = routers.DefaultRouter()
 router.register('notes', NoteViewSet)
@@ -9,4 +9,5 @@ router.register('notes', NoteViewSet)
 urlpatterns = [
     url("^", include(router.urls)),
     url("^auth/register/$", RegistrationAPI.as_view()),
+    url("^auth/login/$", LoginAPI.as_view()),
 ]
