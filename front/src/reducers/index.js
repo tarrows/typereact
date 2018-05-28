808609bd4f26da68ff584ef6e6f18e8f63089a96
c10t <c10t@fa.ke>
2018-05-29 07:54:02 +0900

    add authentication reducer

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import notes from "./notes";
+import auth from "./auth";
 
 
 const typeApp = combineReducers({
-  notes,
+  notes, auth,
 })
 
 export default typeApp;
